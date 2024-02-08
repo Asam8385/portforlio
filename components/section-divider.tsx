9c7713a04851448e3aaa44edb782cc253a496c9e
Asam8385 <subairasam8733260@gmail.com>
2024-02-08 12:13:57 +0530

    10th

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function SectionDivider() {
   return (
     <motion.div
-      className="mb-10 h-16 w-1 rounded-full bg-opacity-20"
+      className="mb-10 h-16 w-1 rounded-full bg-white"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
